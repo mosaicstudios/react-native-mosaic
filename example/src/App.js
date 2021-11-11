5f3c1b5650385985df0037a7ec0788bf27ffad1c
AdamLee321 <adam123dublin@gmail.com>
2021-11-11 18:11:08 +0000

    refactor: log function removed

--- example/src/App.js
+++ example/src/App.js
@@ -59,7 +59,6 @@ export default class App extends Component {
           showCountryPicker
           manualInputType="border"
           onPlaceSelected={(data) => {
-            console.log('data62', data);
             this.setState({ location: data });
           }}
         />
